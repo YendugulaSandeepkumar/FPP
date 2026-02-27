d71ff3fdd7379ecf20bb5bea8d9c5992d81b06d0
YendugulaSandeepkumar <132835261+YendugulaSandeepkumar@users.noreply.github.com>
2026-02-27 20:40:47 +0530

    fixed port binding for render

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const { initDB } = require('./db');
 const routes = require('./routes/api');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
